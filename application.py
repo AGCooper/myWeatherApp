91978cba260f4155a042a625bfd9baff1c6e87c0
AGCooper <alexander.g.cooper@gmail.com>
2019-06-18 15:55:11 -0400

    Adds first commit

---
+++ application.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import sys
+import json
+import requests
+
+def parse_json(json_object):
+
+    # {"slip": {"advice":"If you need cheering up, try searching online for photos of kittens.","slip_id":"47"}}
+    parsed_json = json.loads(json_object)
+    periods = parsed_json['properties']['periods']
+    count = len(periods)
+    count -= 1
+    while count > -1:
+        #print count
+        print periods[count]
+        count -= 1
+    return count
+
+def main():
+
+    ####################
+    # Define variables #
+    ####################
+    url = "https://api.weather.gov/gridpoints/FFC/51,87/forecast/hourly"
+    #################
+    # Make API call #
+    #################
+    r = requests.get(url)
+    ###############
+    # Get results #
+    ###############
+    json_object = r.content
+    api_status = r.status_code
+    api_url = r.url
+    #################
+    # Print results #
+    #################
+    print api_url
+    print api_status
+    result = parse_json(json_object)
+    print result
+
+if __name__=="__main__":
+    sys.exit(main())
